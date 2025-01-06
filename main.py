5dad8793cc244b22befca728402cb21eda1a14e6
Vannilaorange <benscurlockk@gmail.com>
2025-01-06 16:43:18 -0600

    Update main.py
    Added a round up function, I don't know if it works though

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-  # Select a random amount of beats.
-beats = random.randrange(200, 400)
+  import random
+# Select a random amount of beats.
+  beats = random.randrange(200, 400)
 print(beats)
-  # Figure out the corresponding amount of measures.
-(measures1) = beats / 4
+# Figure out the corresponding amount of measures.
+  (measures1) = beats / 4
+# This SHOULD round measures1 up to the nearest whole number
+  math.ceil(measures1)
