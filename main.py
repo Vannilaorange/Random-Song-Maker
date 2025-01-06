1f3afe9f2d9d9f56a240e3977fa03cf57c48dcf8
Vannilaorange <benscurlockk@gmail.com>
2025-01-06 17:08:48 -0600

    Update main.py
    Just tidied up a little

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# THIS ENTIRE FILE DECIDES THE AMOUNT OF BEATS THEN CALCULATES THE AMOUNT OF MEASURES.
 import random
 import math
 # Select a random amount of beats.
@@ -6,6 +7,7 @@ print(beats)
 # Figure out the corresponding amount of measures.
 (measures1) = beats / 4
 print (measures1)
-# This SHOULD round measures1 up to the nearest whole number
+# This SHOULD round measures1 up to the nearest whole number.
 measures2 = math.ceil(measures1)
 print (measures2)
+# The python part is done now, it shouldn't require any more editing.
