01b39739918099b002d6f61446f7d58efa9df866
Vannilaorange <benscurlockk@gmail.com>
2025-01-06 16:48:17 -0600

    Update main.py
    I fixed a bunch of errors in the code, now (measures1) rounds up to the nearest whole number and prints as (measures2)

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import random
+import random
+import math
 # Select a random amount of beats.
-  beats = random.randrange(200, 400)
+beats = random.randrange(200, 400)
 print(beats)
 # Figure out the corresponding amount of measures.
-  (measures1) = beats / 4
+(measures1) = beats / 4
+print (measures1)
 # This SHOULD round measures1 up to the nearest whole number
-  math.ceil(measures1)
+measures2 = math.ceil(measures1)
+print (measures2)
