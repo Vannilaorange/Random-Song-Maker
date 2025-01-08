8116e7ff054fdd72559cf316ad70fe8cd514dea8
Ben <benscurlockk@gmail.com>
2025-01-07 19:08:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,3 @@ print (measures1)
 # This SHOULD round measures1 up to the nearest whole number.
 measures2 = math.ceil(measures1)
 print (measures2)
-# Generate a .csd file
-csd_content = f"""
-<CsoundSynthesizer>
-<CsOptions>
-; Select audio/midi flags here according to platform
--odac ;;;RT audio out
-</CsOptions>
-<CsInstruments>
-instr 1
-    a1 oscil 0.5, 440, 1
-    out a1
-endin
-</CsInstruments>
-<CsScore>
-f 1 0 1024 10 1
-i 1 0 {measures2} 
-</CsScore>
-</CsoundSynthesizer>
-"""
-
-with open("output.csd", "w") as file:
-    file.write(csd_content)
